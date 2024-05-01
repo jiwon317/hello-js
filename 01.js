f6cc19d47e15b0e94d83611d0c019d5786e2fc4a
jiwon317 <chlghqlt12@naver.com>
2024-05-01 13:34:32 +0900

    init

---
+++ 01.js
@@ -0,0 +1,26 @@
+console.log("hi~");
+ 
+// 자바스크립트 파일을 실행시키기 위한 런타임 환경이 nodejs 다
+// java => 컴파일언어 
+// js => 인터프리터언어
+
+// var a = 1;
+
+// String str = "123";
+var str = '123';
+
+// var const let
+// var 
+
+// cosnt let
+
+let a = 1;
+
+a = 2;
+
+// 선언과 할당 이후 해당 식별자에 재할당 불가능
+// 선완과 할당을 동시에 해야함
+// const bc;
+const b = 1;
+
+b = 4;
